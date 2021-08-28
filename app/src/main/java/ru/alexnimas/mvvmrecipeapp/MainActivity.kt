71137721f9b519c962f3a8c8a0afd200be44e7a4
Alexey <mail@alexnimas.ru>
2021-08-28 13:44:34 +0300

    some compose experiments

--- app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package ru.alexnimas.mvvmrecipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = null,
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Happy Meal", fontSize = 30.sp)
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "800 Calories")
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "$5.99", fontSize = 18.sp, color = Color(0xFF85bb65))
+                }
+            }
+        }
+    }
+
+    @Preview(name = "qwer")
+    @Composable
+    fun Greeting(name: String = "Piotr") {
+        Text(text = "Hello $name")
     }
 }
