ffc2d7aa6e590abc1320ad54ccdde2d97b15cb65
Alexey <mail@alexnimas.ru>
2021-08-28 21:01:30 +0300

    some more experiments with compose

--- app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
@@ -3,13 +3,15 @@ package ru.alexnimas.mvvmrecipeapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -32,11 +34,30 @@ class MainActivity : AppCompatActivity() {
                     contentScale = ContentScale.Crop
                 )
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Happy Meal", fontSize = 30.sp)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Happy Meal",
+                            fontSize = 30.sp
+                        )
+                        Text(
+                            text = "$5.99",
+                            fontSize = 18.sp,
+                            color = Color(0xFF85bb65),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
                     Spacer(modifier = Modifier.padding(top = 10.dp))
                     Text(text = "800 Calories")
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "$5.99", fontSize = 18.sp, color = Color(0xFF85bb65))
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "Order now")
+                    }
                 }
             }
         }
