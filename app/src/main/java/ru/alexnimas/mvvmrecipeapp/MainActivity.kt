cf8c655d6ae03adfde73cec00387d039c8644856
Alexey <mail@alexnimas.ru>
2021-08-29 10:05:39 +0300

    some more experiments with compose

--- app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/MainActivity.kt
@@ -1,71 +1,17 @@
 package ru.alexnimas.mvvmrecipeapp
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setContent {
-            Column {
-                Image(
-                    painter = painterResource(id = R.drawable.happy_meal_small),
-                    contentDescription = null,
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
-                )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Happy Meal",
-                            fontSize = 30.sp
-                        )
-                        Text(
-                            text = "$5.99",
-                            fontSize = 18.sp,
-                            color = Color(0xFF85bb65),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "800 Calories")
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Button(
-                        onClick = {},
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Order now")
-                    }
-                }
-            }
-        }
-    }
-
-    @Preview(name = "qwer")
-    @Composable
-    fun Greeting(name: String = "Piotr") {
-        Text(text = "Hello $name")
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/RecipeListFragment.kt
@@ -0,0 +1,26 @@
+package ru.alexnimas.mvvmrecipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class RecipeListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text("Hey look a composable")
+            }
+        }
+        return view
+    }
+}
