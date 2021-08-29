13333b0c0673f3222ed1f6dc0249566d47ae89dc
Alexey <mail@alexnimas.ru>
2021-08-29 10:23:29 +0300

    some more experiments with compose and navigation

--- app/src/main/java/ru/alexnimas/mvvmrecipeapp/RecipeListFragment.kt
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/RecipeListFragment.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class RecipeListFragment : Fragment() {
 
@@ -15,12 +23,23 @@ class RecipeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = ComposeView(requireContext())
-        view.apply {
+        return ComposeView(requireContext()).apply {
             setContent {
-                Text("Hey look a composable")
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Recipe List",
+                        fontSize = 40.sp
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(
+                        onClick = {
+                            findNavController().navigate(R.id.viewRecipe)
+                        }
+                    ) {
+                        Text(text = "To Recipe Fragment")
+                    }
+                }
             }
         }
-        return view
     }
 }
