1b867d91963bd9eb88e9c337f46535f637a295bb
Alexey <mail@alexnimas.ru>
2021-09-06 18:29:56 +0300

    Added EntityMapper and RecipeNetworkMapper

---
+++ app/src/main/java/ru/alexnimas/mvvmrecipeapp/domain/util/EntityMapper.kt
@@ -0,0 +1,8 @@
+package ru.alexnimas.mvvmrecipeapp.domain.util
+
+interface EntityMapper<Entity, DomainModel> {
+
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
